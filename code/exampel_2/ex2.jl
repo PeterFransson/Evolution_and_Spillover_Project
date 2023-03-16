d0613ea9018e1ca5397562ad8095239df2fd595a
PeterFransson <peter.fransson@gmail.com>
2023-03-16 07:35:53 +0100

    added mutation distribution

--- code/exampel_2/ex2.jl
+++ code/exampel_2/ex2.jl
@@ -5,7 +5,9 @@ normal_d(x,μ,σ²) = exp(-(x-μ)^2/(σ²*2))/sqrt(2*π*σ²)
 γ_fun(x,μ,σ²,amplitude) = amplitude*exp(-(x-μ)^2/(σ²*2))
 
 function run_ex2()
-    trait_line = range(0.0,stop=1.0,length=200)
+    nₚ = 100
+    Δₚ = 1/nₚ
+    trait_line = range(0.0,stop=1.0,length=nₚ)
     μ_A = 0.2
     μ_B = 0.3
     σ² = 0.0025
@@ -14,14 +16,19 @@ function run_ex2()
     Eβ₀ = 0.3 #Mean infection rate, β, in the infected population, β~N(Eβ,σ²β)
     σβ₀  = 0.05 #Standard deviation of β~N(Eβ,σ²β) 
     σ²β₀  = σβ₀^2 #Variance of β~N(Eβ,σ²β)
-    σ²ₘ = 0.01^2 #Variance of the mutan trait distribution within strain i infected individual
-                 #βᵢₘ~N(βᵢ+δₘ,σ²ₘ)
-    δₘ = 0.01 #The offset from  he mutan trait distribution within strain i infected individual
-              #βᵢₘ~N(βᵢ+δₘ,σ²ₘ)
+    σ²ₘ = 0.01^2 #Variance of the mutan distribution within strain i infected individual
+                 #βᵢₘ~N(zᵢ+δₘ,σ²ₘ)
+    δₘ = 0.01 #The offset from the mutan distribution within strain i infected individual
+              #βᵢₘ~N(zᵢ+δₘ,σ²ₘ)
     μₘ = 0.05 #mutation rate   
 
     plot(trait_line,γ_fun.(trait_line,Ref(μ_A),Ref(σ²),Ref(0.6)),xlabel="Pathogen Trait",ylabel="γ",label="A")
     plot!(trait_line,γ_fun.(trait_line,Ref(μ_B),Ref(σ²),Ref(0.6)),label="B")
+
+    m(z,z_vec,μₘ,σ²ₘ) = normal_d(z,μₘ,σ²ₘ)/sum(normal_d.(z_vec,Ref(μₘ),Ref(σ²ₘ)))
+
+    @show cc = sum(normal_d.(trait_line,Ref(μ_A),Ref(σ²)))  
+    plot(trait_line,normal_d.(trait_line,Ref(μ_A),Ref(σ²))/cc,xlabel="Pathogen Trait",ylabel="Mutations Pr")
 end
 
 run_ex2()
