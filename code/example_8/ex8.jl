ed5139c03f38c62ff28916ba2e4b961b8e6db202
PeterFransson <peter.fransson@gmail.com>
2023-06-05 16:25:40 +0200

    Updated ex8

--- code/example_8/ex8.jl
+++ code/example_8/ex8.jl
@@ -52,74 +52,61 @@ function SI_Gillespie!(S::Vector{U},
     t,
     p) where {U<:Integer,T<:Integer}
 
-    c,μₘ,σₘ,γ,K,N,t_end = p
-    if isempty(I)==false
+    c,μₘ,σₘ,γ,K,N,c_m,t_end = p
+    
         n_pathogen_strains = length(I)        
 
         r_num = rand(n_pathogen_strains,n_species,3)
         Δt = Inf
-        pathogen_strains = 1
-        species = 1
-        state = 1 
-        
-        r_mat = zeros()
+        pathogen_strains = 0
+        species = 0
+        state = 0       
 
-        for i in 1:n_pathogen_strains #Pathogen varriant loop
-            I_z = I[i].number
-            strategy_z = I[i].strategy
-            for j in 1:n_species #species loop                       
-                 
-                rᵣ = I_z[j]*c
-                rₘ = I_z[j]*μₘ
-                rᵢ = S[j]*γ[j](strategy_z)*sum(K[j,:].*I_z./N)
+    for i in 1:n_pathogen_strains #Pathogen varriant loop
+        I_z = I[i].number
+        strategy_z = I[i].strategy
+        for j in 1:n_species #species loop                       
+                
+            rᵣ = I_z[j]*c
+            rₘ = I_z[j]*μₘ
+            rᵢ = S[j]*γ[j](strategy_z)*sum(K[j,:].*I_z./N)
 
-                τ = [τ_fun(rᵣ,r_num[i,j,1]),τ_fun(rₘ,r_num[i,j,2]),τ_fun(rᵢ,r_num[i,j,3])]
-                state_temp = argmin(τ)
-                if τ[state_temp]<Δt
-                    Δt = τ[state_temp]
-                    pathogen_strains = i
-                    state = state_temp
-                    species = j
-                end                
-            end       
-        end
-
-        if state==1 #recover
-            I_z = I[pathogen_strains].number
-            if sum(I_z)-1>0
-                I[pathogen_strains].number[species] -= 1
-            else
-                popat!(I,pathogen_strains)
-            end
-            S[species]+=1
-            return t+Δt 
-        elseif state==2 #mutation     
-            I_z = I[pathogen_strains].number      
-            resident_strategy = I[pathogen_strains].strategy
-            dₘ = Normal(resident_strategy,σₘ)
-            mutant_strategy = min(max(rand(dₘ),0.0),1.0)
-            if γ[species](mutant_strategy)>γ[species](resident_strategy)
-                I_z_mutant = zeros(eltype(I_z),n_species)
-                I_z_mutant[species]+=1
-                push!(I,Infected(I_z_mutant,mutant_strategy))
-                if sum(I_z)-1>0
-                    I[pathogen_strains].number[species] -= 1
-                else
-                    popat!(I,pathogen_strains)
-                end
-            end
-            return t+Δt 
-        elseif state==3 #infection
-            I[pathogen_strains].number[species] += 1
-            S[species]-=1            
-
-            return t+Δt 
-        else
-            error("State error: sate either i, m, or r")
-        end
-    else
-        return t_end
+            τ = [τ_fun(rᵣ,r_num[i,j,1]),τ_fun(rₘ,r_num[i,j,2]),τ_fun(rᵢ,r_num[i,j,3])]
+            state_temp = argmin(τ)
+            if τ[state_temp]<Δt
+                Δt = τ[state_temp]
+                pathogen_strains = i
+                state = state_temp
+                species = j
+            end                
+        end       
     end
+
+    if state==1 #recover            
+        I[pathogen_strains].number[species] -= 1            
+        S[species]+=1
+        return t+Δt 
+    elseif state==2 #mutation               
+        resident_strategy = I[pathogen_strains].strategy
+
+        r_rnd = rand()            
+        pathogen_strains_mutant = find_weighted_idx(r_rnd,c_m[pathogen_strains,:]) 
+        mutant_strategy = I[pathogen_strains_mutant].strategy
+
+        if γ[species](mutant_strategy)>γ[species](resident_strategy)
+            I[pathogen_strains_mutant].number[species]+=1
+            I[pathogen_strains].number[species] -= 1                
+        end
+        return t+Δt 
+    elseif state==3 #infection
+        I[pathogen_strains].number[species] += 1
+        S[species]-=1            
+
+        return t+Δt 
+    else
+        println("State error or extinction: sate either i, m, or r")
+        return t_end
+    end   
 end
 
 function run_Gillespie!(S::Vector{T},
@@ -135,14 +122,12 @@ function run_Gillespie!(S::Vector{T},
     push!(S_vec,copy(S))
 
     I_vec = typeof(I)[] 
-    push!(I_vec,deepcopy(I)) 
-      
-    n_variants = [length(I)]
-    mean_strategy = [mean([strain.strategy for strain in I])]
+    push!(I_vec,deepcopy(I))     
     
     Δt_sample = (t_end-t₀)/n_samples
     sample_nr = 1
     n_species = length(S)
+
     while t<t_end
         t = SI_Gillespie!(S,I,n_species,t,p)    
 
@@ -150,19 +135,12 @@ function run_Gillespie!(S::Vector{T},
             println("$(sample_nr)/$(n_samples)")
             push!(S_vec,copy(S))         
             push!(t_vec,t) 
-            push!(n_variants,length(I))
-            if  length(I)>0
-                push!(mean_strategy,mean([infected.strategy for infected in I]))                
-                push!(I_vec,deepcopy(I))                  
-            else
-                push!(mean_strategy,0.0)
-                push!(I_vec,Infected[])
-            end
+            push!(I_vec,deepcopy(I))          
             sample_nr += 1            
         end       
     end 
         
-    return (S_vec,I_vec,t_vec,n_variants,mean_strategy)
+    return (S_vec,I_vec,t_vec)
 end
 
 function draw_strain_distribution(t_vec,I_vec,img_name)
@@ -180,17 +158,26 @@ function draw_strain_distribution(t_vec,I_vec,img_name)
         legends=false,
         xlim=(0,1))        
     end
-    gif(strain_distribution, "./fig/ex7/"*img_name*".gif", fps = 15)      
+    gif(strain_distribution, "./fig/ex8/"*img_name*".gif", fps = 15)      
 end
 
 function draw_strain_evolution_plot(t_vec,I_vec,t₀,t_end,img_name)
     plt = plot(ylims=(0.0,1.0),xlims=(t₀,t_end),legends=false)
-    for (time,strain_dist) in zip(t_vec,I_vec) 
-        strategies = [strain.strategy for strain in strain_dist]
-        time_vec = [time for strain in strain_dist]
-        plot!(plt,time_vec,strategies,seriestype=:scatter)              
+    for (time,strain_dist) in zip(t_vec,I_vec)
+        
+        strategies = []
+        for strain in strain_dist
+            if sum(strain.number)>0
+                push!(strategies,strain.strategy)
+            end
+        end
+
+        if isempty(strategies) == false
+            time_vec = [time for i in strategies]
+            plot!(plt,time_vec,strategies,seriestype=:scatter) 
+        end             
     end    
-    savefig(plt,"./fig/ex7/"*img_name*".svg")
+    savefig(plt,"./fig/ex8/"*img_name*".svg")
 end
 
 function draw_compartments(t_vec,I_vec,S_vec,img_name)    
@@ -210,12 +197,12 @@ function draw_compartments(t_vec,I_vec,S_vec,img_name)
         plot!(plt,t_vec,S_plot_mat[:,species_idx])
         plot!(plt,t_vec,I_plot_mat[:,species_idx])
     end
-    savefig(plt,"./fig/ex7/"*img_name*".svg")
+    savefig(plt,"./fig/ex8/"*img_name*".svg")
 end
 
 function run_ex8()    
     Nₚ = 100 #Number of strains 
-    z_vec = range(0.0,stop=1.0,length=Nₚ+1)
+    z_vec = z_fun.(1:Nₚ,Ref(Nₚ))
 
     μ_a,σ²_a,amplitude_a = 0.2,0.0025,0.6
     μ_b,σ²_b,amplitude_b = 0.4,0.0025,0.6 #0.3
@@ -235,7 +222,8 @@ function run_ex8()
     c = 0.1
     μₘ = 0.05
 
-    z_start = 0.2    
+    @show z_start_idx = floor(Int64,0.2*Nₚ)
+    @show z_start = z_fun(z_start_idx,Nₚ) 
 
     #Initial states
     N_a = 10^3     
@@ -244,19 +232,22 @@ function run_ex8()
     N_b = 10^3     
     I_b₀ = 0
     S₀_b = N_b-I_b₀ 
-    σₘ = 0.05#0.0158
+    σₘ = 0.0158#0.0158
 
     #Calculate mutation matrix
     m,c_m = create_mutation_matrix(Nₚ,σₘ)
-
-    #=
-    S = [S₀_a,S₀_b] 
-    I = [Infected([I_a₀,I_b₀],z_start)]  
+    
+    S = [S₀_a,S₀_b]     
+    I = [Infected([0,0],z_fun(i,Nₚ) ) for i in 1:Nₚ]  
+    I[z_start_idx].number[1] = I_a₀
+    I[z_start_idx].number[2] = I_b₀
+    @show I[z_start_idx]
 
     @show I_a_eq =  (1-c/(γ_a(μ_a)*K_aa))*N_a
     
     @show γ_a.(z_start)*K_aa/c
     @show γ_b.(z_start)*K_bb/c
+
     
     #Plot R₀
     plot(z_vec,γ_a.(z_vec)*K_aa/c,xlabel="Strategy",ylabel="R₀") 
@@ -270,16 +261,17 @@ function run_ex8()
     γ = (γ_a,γ_b)
     N = [N_a,N_b]
 
-    p = c,μₘ,σₘ,γ,K,N,t_end
+    p = c,μₘ,σₘ,γ,K,N,c_m,t_end
 
     n_samples = 50
 
-    S_vec,I_vec,t_vec,n_variants,mean_strategy = run_Gillespie!(S,I,t₀,p,n_samples)     
+    S_vec,I_vec,t_vec = run_Gillespie!(S,I,t₀,p,n_samples)     
+    
     
     draw_strain_distribution(t_vec,I_vec,"strain_evo")
     draw_strain_evolution_plot(t_vec,I_vec,t₀,t_end,"trait_vs_time")
     draw_compartments(t_vec,I_vec,S_vec,"compartments")    
-
+    #=
     pl1 = plot(t_vec,mean_strategy)
     pl1 = plot!([t₀,t_end],[μ_a,μ_a])
     pl1 = plot!([t₀,t_end],[μ_b,μ_b])
