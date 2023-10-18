6b881340027e68a3d3cd8e55328fe1f907e4bd59
PeterFransson <peter.fransson@gmail.com>
2023-10-18 15:16:47 +0200

    added create TEP

--- code/AD_2_Species/analysis.jl
+++ code/AD_2_Species/analysis.jl
@@ -1,7 +1,7 @@
 function test()
     Nₚ = 1000
     z_start = 0.1
-    z_end = 0.5
+    z_end = 0.4
 
     #Parameters
     c_aa = 0.425
@@ -24,32 +24,58 @@ function test()
 
     τ_a(z) = τ_fun(z,μ_a,σ²,amplitude)
     τ_b(z) = τ_fun(z,μ_b,σ²,amplitude)
+    τ_prim_a(z) = τ_prim_fun(z,μ_a,σ²,amplitude)
+    τ_prim_b(z) = τ_prim_fun(z,μ_b,σ²,amplitude)
 
     t_start = 0
-    t_end = 6000
+    t_end = 7000
     tspan = (t_start,t_end)
 
     u₀ = [S₀_a I_a₀;S₀_b I_b₀]    
 
     option = z_start,z_end,Nₚ,u₀,tspan 
-
-    #p_in = τ_a,τ_b,c_aa,c_bb,c_ab,γ,γ,N_a,N_b,μ_a,μ_b
-    strategy = 0.32
-    p_strategy = τ_a(strategy),τ_b(strategy),c_aa,c_bb,c_ab,γ,γ,N_a,N_b
-
-    R = system_matrix(p_strategy,(S₀_a,S₀_b))
-    @show eigvals(R)
     
-    eq_point = find_eq(u₀,tspan,p_strategy)
+    #=
+    z_r = 0.25
+    p_system = τ_a(z_r),τ_b(z_r),c_aa,c_bb,c_ab,γ,γ,N_a,N_b
+    eq = find_eq(u₀,tspan,p_system)
+    S = (eq.S_a,eq.S_b)
 
-    S_eq = (eq_point.S_a,eq_point.S_b)
-    R = system_matrix(p_strategy,S_eq)
-    @show eigvals(R)
+    R = system_matrix(p_system,S)
+    @show R_eval = maximum(eigvals(R))
 
-    strategy_new = strategy-0.00001
-    p_strategy = τ_a(strategy_new),τ_b(strategy_new),c_aa,c_bb,c_ab,γ,γ,N_a,N_b
-    R = system_matrix(p_strategy,S_eq)
-    @show eigvals(R)
+    Δz = 0.00001
+    z_m = z_r+Δz 
+    p_system = τ_a(z_m),τ_b(z_m),c_aa,c_bb,c_ab,γ,γ,N_a,N_b
+    R = system_matrix(p_system,S)
+    @show R_eval_new = maximum(eigvals(R))
+
+    @show dλ_max_num = (R_eval_new-R_eval)/Δz
+
+    p_grad = τ_a(z_r),τ_b(z_r),c_aa,c_bb,c_ab,γ,γ,N_a,N_b,τ_prim_a(z_r),τ_prim_b(z_r)
+    calculate_select_grad(p_grad,S) 
+    =# 
+
+    option = z_start,z_end,Nₚ,u₀,tspan 
+    p_in = τ_a,τ_b,c_aa,c_bb,c_ab,γ,γ,N_a,N_b,μ_a,μ_b 
+
+    coex_region = create_coex_region(p_in,option)   
+    z_vec = range(z_start,stop=z_end,length=Nₚ) 
+    heatmap(z_vec,z_vec,coex_region)
+
+    u₀_2_strain = [N_a-2 1 1;N_b-2 1 1]   
+
+    option = z_start,z_end,Nₚ,u₀,tspan,u₀_2_strain
+    p_in = τ_a,τ_b,c_aa,c_bb,c_ab,γ,γ,N_a,N_b,μ_a,μ_b,τ_prim_a,τ_prim_b 
+    tep = create_TEP(p_in,option,coex_region;ϵ=1e-2)
+
+    heatmap(z_vec,z_vec,tep) 
+    plot!([z_start,z_end],[z_start,z_end],c=:green)
+    plot!([μ_a,μ_a],[z_start,z_end],c=:red)
+    plot!([z_start,z_end],[μ_a,μ_a],c=:red)
+    plot!([z_start,z_end],[μ_b,μ_b],c=:blue)
+    plot!([μ_b,μ_b],[z_start,z_end],legend=false,c=:blue)
+    savefig("./fig/AD_2_Species/tep.svg")    
 end
 
 test()
--- code/AD_2_Species/code.jl
+++ code/AD_2_Species/code.jl
@@ -42,6 +42,45 @@ end
 
 normal_d(x,μ,σ²) = exp(-(x-μ)^2/(σ²*2))/sqrt(2*π*σ²)
 τ_fun(x,μ,σ²,amplitude) = amplitude*exp(-(x-μ)^2/(σ²*2))
+τ_prim_fun(x,μ,σ²,amplitude) = -amplitude*exp(-(x-μ)^2/(σ²*2))*2*(x-μ)/(σ²*2)
+
+#Calculate Selection gradients
+function calculate_select_grad(S_a,S_b,τ_a,τ_b,τ_prim_a,τ_prim_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b)
+    r_11 = S_a*τ_a*c_aa/N_a-γ_a
+    r_12 = S_a*τ_a*c_ab/N_b
+    r_21 = S_b*τ_b*c_ab/N_a
+    r_22 = S_b*τ_b*c_bb/N_b-γ_b
+
+    dr_11 = S_a*τ_prim_a*c_aa/N_a
+    dr_12 = S_a*τ_prim_a*c_ab/N_b
+    dr_21 = S_b*τ_prim_b*c_ab/N_a
+    dr_22 = S_b*τ_prim_b*c_bb/N_b
+
+    R = [r_11 r_12;r_21 r_22]
+    dR = [dr_11 dr_12;dr_21 dr_22]
+
+    R_e_val,R_e_vec_R = eigen(R)
+    R_e_val,R_e_vec_L = eigen(transpose(R))
+
+    R_e_vec_L_T = transpose(R_e_vec_L[:,2])
+
+    dλ_max_alt = R_e_vec_L_T*dR*R_e_vec_R[:,2]/(R_e_vec_L_T*R_e_vec_R[:,2])
+
+    tr_R = r_11+r_22
+    det_R =  r_11*r_22-r_12*r_21
+
+    dtr_R = dr_11+dr_12
+    ddet_R = dr_11*r_22+r_11*dr_22-dr_12*r_21-r_12*dr_21
+
+    dλ_max = (dtr_R+(2*tr_R*dtr_R-4*ddet_R)/(2*sqrt(tr_R^2-4*det_R )))/2
+    return dλ_max
+end
+function calculate_select_grad(p,S)    
+    τ_a,τ_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,τ_prim_a,τ_prim_b = p
+    S_a,S_b = S[1],S[2]
+    dλ_max = calculate_select_grad(S_a,S_b,τ_a,τ_b,τ_prim_a,τ_prim_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b)
+    return dλ_max
+end
 
 #Matrix (R) describing the dynamics of the two-species-one-pathogen system 
 #dI = RI
@@ -59,7 +98,7 @@ end
 
 #Check satablility of system matrix of the two-species-one-pathogen system 
 function check_system_matrix(p,S)    
-    system_matrix(p,S)
+    R = system_matrix(p,S)
     eigs = eigvals(R)
     return any(real.(eigs).>0)
 end
@@ -91,6 +130,37 @@ function find_eq(u₀,tspan,p)
     return eq_point
 end
 
+#System dynamics for the two-species-two-pathogen system
+function epievodyn_simple_two_strain!(du,u,p,t)
+    τ_a,τ_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,strain_1,strain_2 = p
+  
+    S = u[1:2,1]
+    I_1 = u[1:2,2]
+    I_2 = u[1:2,3]
+
+    R_1 = system_matrix(S[1],S[2],τ_a(strain_1),τ_b(strain_1),c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b) 
+    R_2 = system_matrix(S[1],S[2],τ_a(strain_2),τ_b(strain_2),c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b) 
+
+    du[1:2,1] = -R_1*I_1-R_2*I_2
+    du[1:2,2] = R_1*I_1
+    du[1:2,3] = R_2*I_2 
+end
+
+#Function to find the equilibrium point for the two-species-two-pathogen system
+function find_eq_2_strain(u₀,tspan,p)
+    t_end = tspan[2] 
+    prob = ODEProblem(epievodyn_simple_two_strain!,u₀,tspan,p) #Setup the ODE problem
+    sol = solve(prob) #Solve the ODE problem, sol contains a continuous approximation to the ODE
+
+    S_a_eq,S_b_eq = sol(t_end)[1,1],sol(t_end)[2,1]
+    I_a_eq,I_b_eq = sol(t_end)[1,2]+sol(t_end)[1,3],sol(t_end)[2,2]+sol(t_end)[2,3]
+
+    eq_point = EqPoint(S_a_eq,S_b_eq,I_a_eq,I_b_eq)
+
+    return eq_point
+end
+
+
 #Calcualte the R0 (reproduction number disease free state) for the two-species-one-pathogen system 
 function calc_R0(p)
     τ_a,τ_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b = p
@@ -109,15 +179,7 @@ function calc_R0_m(p,S_a_eq,S_b_eq)
     return abs((tr(K_L)+sqrt(tr(K_L)^2-4*det(K_L)))/2)
 end
  
-function draw_PIP(fig_name,strategies,r_m_mat,z_start,z_end,z_a,z_b)
-    heatmap(strategies,strategies,r_m_mat)
-    plot!([z_start,z_end],[z_start,z_end],c=:green)
-    plot!([z_a,z_a],[z_start,z_end],c=:red)
-    plot!([z_b,z_b],[z_start,z_end],legend=false,c=:blue)
-    savefig(fig_name) 
-    return nothing
-end
-function draw_PIP(fig_name,p_in,option)
+function create_PIP(p_in,option)
     z_start,z_end,Nₚ,u₀,tspan = option
     τ_a,τ_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,z_a,z_b = p_in
 
@@ -150,9 +212,93 @@ function draw_PIP(fig_name,p_in,option)
             r_m_mat_alt[:,col_idx].=0.5
         end        
     end
+    return r_m_mat,r_m_mat_alt
+end
+
+function draw_PIP(fig_name,strategies,r_m_mat,z_start,z_end,z_a,z_b)
+    heatmap(strategies,strategies,r_m_mat)
+    plot!([z_start,z_end],[z_start,z_end],c=:green)
+    plot!([z_a,z_a],[z_start,z_end],c=:red)
+    plot!([z_b,z_b],[z_start,z_end],legend=false,c=:blue)
+    savefig(fig_name) 
+    return nothing
+end
+function draw_PIP(fig_name,p_in,option)
+    r_m_mat,r_m_mat_alt = create_PIP(p_in,option)
 
     draw_PIP(fig_name*".svg",strategies,r_m_mat,z_start,z_end,z_a,z_b)
     draw_PIP(fig_name*"_alt.svg",strategies,r_m_mat_alt,z_start,z_end,z_a,z_b)
     
     return nothing
+end
+
+function create_coex_region(pip::Matrix{T}) where{T}
+    n_row,n_col = size(pip)
+    n_row==n_col||error("n_row!=n_col")
+    coex_region = zeros(T,n_row,n_col)
+    for row in 1:n_row
+        for col in 1:n_col 
+            r_1 = pip[row,col]
+            r_2 = pip[col,row]
+            if r_1≈0.5
+                coex_region[row,col] = 0.5
+            else
+                if r_1≈1.0&&r_2≈1.0
+                    coex_region[row,col]=1
+                else
+                    coex_region[row,col]=0
+                end
+            end
+        end
+    end
+    return coex_region
+end
+
+function create_coex_region(p_in,option)
+    r_m_mat,r_m_mat_alt = create_PIP(p_in,option)
+    coex_region = create_coex_region(r_m_mat)
+    return coex_region
+end
+
+function create_TEP(p_in,option,coex_region::Matrix{T};ϵ::Real=1e-6) where{T}
+    z_start,z_end,Nₚ,u₀,tspan,u₀_2_strain = option
+    τ_a,τ_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,z_a,z_b,τ_prim_a,τ_prim_b = p_in
+    
+    strategies = range(z_start,stop=z_end,length=Nₚ)
+
+    (n_row,n_col) = size(coex_region)
+    n_row==n_col==Nₚ||error("n_row!=n_col!=Nₚ")    
+
+    tep = zeros(T,n_row,n_col)
+
+    for row in 1:n_row
+        for col in 1:n_col            
+            if coex_region[row,col]≈0.5
+                tep[row,col] = 0.5
+            elseif coex_region[row,col]≈0
+                tep[row,col] = 0
+            else
+                r_1 = strategies[row] #Resident strain 1
+                r_2 = strategies[col] #Resident strain 2
+                p_system = τ_a,τ_b,c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,r_1,r_2
+                eq = find_eq_2_strain(u₀_2_strain,tspan,p_system)
+
+                p_grad_1 = τ_a(r_1),τ_b(r_1),c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,τ_prim_a(r_1),τ_prim_b(r_1)
+                p_grad_2 = τ_a(r_2),τ_b(r_2),c_aa,c_bb,c_ab,γ_a,γ_b,N_a,N_b,τ_prim_a(r_2),τ_prim_b(r_2)
+
+                dλ_max_1 = calculate_select_grad(p_grad_1,(eq.S_a,eq.S_b))
+                dλ_max_2 = calculate_select_grad(p_grad_2,(eq.S_a,eq.S_b))
+
+                if abs(dλ_max_1)<ϵ&&abs(dλ_max_2)<ϵ
+                    tep[row,col]=0.2
+                elseif abs(dλ_max_1)<ϵ||abs(dλ_max_2)<ϵ
+                    tep[row,col]=0.7
+                else
+                    tep[row,col]=1
+                end
+            end
+        end
+    end
+
+    return tep
 end
